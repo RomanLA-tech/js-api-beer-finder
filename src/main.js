aa97c9fa2dc89ad431fb3bb8770dd1c559c0e2c5
Roman Lagonsky <roman.workmail4@gmail.com>
2022-12-20 13:52:58 +0100

    #US-002 Added ability to search

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import '../src/styles.css'
+import '../src/styles.css';
+import {SEARCH_FORM} from './consts';
+import {getProducts} from './utils';
+
+window.addEventListener('load', () => {
+	
+	SEARCH_FORM.addEventListener('submit', (e) => {
+		e.preventDefault();
+		getProducts();
+	});
+});
